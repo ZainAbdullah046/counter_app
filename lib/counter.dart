04a17b0db1f37c34a84086526b6668a460314125
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-03-30 01:21:09 +0500

    final counter app

--- lib/counter.dart
+++ lib/counter.dart
@@ -16,21 +16,28 @@ class _MyWidgetState extends State<counter> {
           title: const Center(
               child: Text(
             "COUNTER APP",
-            style: TextStyle(fontSize: 50),
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white70),
           )),
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.black87,
           shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(20)))),
       body: DecoratedBox(
         decoration: const BoxDecoration(
-            image: DecorationImage(image: NetworkImage(""))),
+            image: DecorationImage(
+          image: AssetImage('assest/images/count.jpg'),
+          fit: BoxFit.cover,
+        )),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 count.toString(),
-                style: TextStyle(fontSize: 50),
+                style:
+                    const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
               ElevatedButton(
                   onPressed: () {
@@ -38,7 +45,11 @@ class _MyWidgetState extends State<counter> {
                       count++;
                     });
                   },
-                  child: Text("PRESS TO INCREASE")),
+                  child: const Text("PRESS TO INCREASE",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87))),
             ],
           ),
         ),
