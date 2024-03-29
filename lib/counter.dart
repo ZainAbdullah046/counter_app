f1f377c64364befdad623344f3736d6a9392aee8
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-03-30 00:50:15 +0500

    counter

--- lib/counter.dart
+++ lib/counter.dart
@@ -8,14 +8,41 @@ class counter extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<counter> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text("COUNTER APP"),
+          title: const Center(
+              child: Text(
+            "COUNTER APP",
+            style: TextStyle(fontSize: 50),
+          )),
           backgroundColor: Colors.blueAccent,
           shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(20)))),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: NetworkImage(""))),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                count.toString(),
+                style: TextStyle(fontSize: 50),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      count++;
+                    });
+                  },
+                  child: Text("PRESS TO INCREASE")),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
